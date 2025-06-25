63a3c98edb2f9b23f11e4d4cd219eee43b765335
Akponome Francis <francisfuture080@gmail.com>
2025-06-25 21:57:30 +0100

    Updated

--- routes/user_route.js
+++ routes/user_route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import  authMiddleware  from "../middleware/authMiddleWare.js"
+import authMiddleware from "../middleware/authmiddleware.js";
 import { getProfile,updateProfile} from "../controllers/userController.js"
 
 
